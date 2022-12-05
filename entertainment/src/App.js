53311087a8d262593df59485491e329e29e91e8b
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-05 21:27:01 +0530

    add

---
+++ entertainment/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className='header'>📽️📽️Entertainment Hub📽️📽️</span>
+    </div>
+  );
+}
+
+export default App;
